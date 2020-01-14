b2d685d383fe6abd9de322ad7cf4bb3d20c080d1
InKyoJeong <viapolar@naver.com>
2020-01-14 20:14:05 +0900

    Add Gradient

--- src/components/top/top.js
+++ src/components/top/top.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+// import { GitHubIcon } from "../social-share/github-icon"
 import "./top.css"
 
 export const Top = ({ title, location, rootPath }) => {
@@ -8,10 +8,11 @@ export const Top = ({ title, location, rootPath }) => {
   return (
     <div className="top">
       {!isRoot && (
-        <Link to={`/`} className="link">
+        <Link to={`/`} className="link" style={{ boxShadow: `none` }}>
           {title}
         </Link>
       )}
+      {/* <GitHubIcon /> */}
     </div>
   )
 }
---
+++ src/pages/about.js
@@ -0,0 +1,4 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import { rhythm } from "../utils/typography"
