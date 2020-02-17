aa2b8c09094dfea069b12dab30b0c5812b5083bc
InKyoJeong <viapolar@naver.com>
2020-02-17 17:59:26 +0900

    test: Test-2 google Search Console

---
+++ src/pages/googlebf7ade52adaccafb.html.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import "./about.css"
+
+class GoogleSearch extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="GoogleSearch" />
+      </Layout>
+    )
+  }
+}
+
+export default GoogleSearch
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
