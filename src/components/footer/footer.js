f99b4fe24dfc06f3dd5ade2bd0acb0187289869d
InKyoJeong <viapolar@naver.com>
2020-03-02 13:15:54 +0900

    docs: Edit footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,5 +3,7 @@ import React from "react"
 import "./footer.scss"
 
 export const Footer = () => (
-  <footer className="footer">© {new Date().getFullYear()}, Inkyo Jeong</footer>
+  <footer className="footer">
+    Copyright © {new Date().getFullYear()}, <strong>Inkyo Jeong</strong>
+  </footer>
 )
