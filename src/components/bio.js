58779c4b66ade85f652be7219cd647aa233516a0
InKyoJeong <viapolar@naver.com>
2020-03-12 05:03:24 +0900

    docs: Modify Profile Container size

--- src/components/bio.js
+++ src/components/bio.js
@@ -58,7 +58,7 @@ const Bio = () => {
         style={{
           marginRight: rhythm(1 / 2),
           marginBottom: 0,
-          minWidth: 80,
+          minWidth: 70,
           borderRadius: `100%`,
         }}
         imgStyle={{
