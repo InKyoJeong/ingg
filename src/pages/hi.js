c4da9c0fd8dfddd24d4de37b2d58cdb0ccee9b3b
InKyoJeong <viapolar@naver.com>
2020-05-09 21:07:51 +0900

    feat: Add hi page

---
+++ src/pages/hi.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "./hi.css"
+
+class Mincho extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="mincho" />
+        <br></br>
+        <iframe
+          class="video"
+          // width="560"
+          // height="315"
+          src="https://www.youtube.com/embed/guMCTWMD1iE"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+          title="mincho"
+        ></iframe>
+      </Layout>
+    )
+  }
+}
+
+export default Mincho
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
