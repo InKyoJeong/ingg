ecd020c7c72d072858fffdb5548ce3276e68539b
InKyoJeong <viapolar@naver.com>
2020-05-25 12:59:25 +0900

    test: Test iframe video

--- src/pages/hi.js
+++ src/pages/hi.js
@@ -14,15 +14,24 @@ class Mincho extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="mincho" />
         <br />
-        <iframe
+        {/* <iframe
           class="video"
           // width="560"
           // height="315"
           src="https://www.youtube.com/embed/guMCTWMD1iE"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          // allowfullscreen
           title="mincho"
+        ></iframe> */}
+
+        <iframe
+          class="video"
+          src="https://www.youtube.com/embed/um0GNZoTPqk?list=PLcTVu2Bx02SsgTiXno8X9zHcn2aasO2yY?rel=0;autoplay=1;"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          title="taeyeon"
+          // allowfullscreen
         ></iframe>
       </Layout>
     )
