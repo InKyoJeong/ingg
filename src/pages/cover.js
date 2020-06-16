37f9601bce50e8c276cf71695f75d7612ee2e7ca
InKyoJeong <viapolar@naver.com>
2020-06-16 09:05:24 +0900

    docs: Change cover music

--- src/pages/cover.js
+++ src/pages/cover.js
@@ -36,13 +36,22 @@ class Cover extends React.Component {
           // allowfullscreen
           title="mincho"
         ></iframe> */}
-        <iframe
+        {/* <iframe
           class="video"
           src="https://www.youtube.com/embed/um0GNZoTPqk?list=PLcTVu2Bx02SsgTiXno8X9zHcn2aasO2yY?rel=0;autoplay=1;"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           title="taeyeon"
           // allowfullscreen
+        ></iframe> */}
+
+        <iframe
+          class="video"
+          src="https://www.youtube.com/embed/oiBswnuvv80?rel=0;autoplay=1;"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+          title="kwon"
         ></iframe>
       </Layout>
     )
